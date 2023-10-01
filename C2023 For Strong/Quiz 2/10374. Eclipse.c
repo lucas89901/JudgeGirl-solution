54dd7e11ff2c3e2bcf7e0828b2c8d370bf86fe3e
Lucas Lu <lucas89901@gmail.com>
2023-10-01 15:50:49 +0800

    Improve solution of "10374. Eclipse"

--- C2023 For Strong/Quiz 2/10374. Eclipse.c
+++ C2023 For Strong/Quiz 2/10374. Eclipse.c
@@ -1,9 +1,10 @@
-#include <stdbool.h>
 #include <stdio.h>
 
-// Check if point (px, py) is in a shadow with a bottom-left point of (x, y).
-inline bool IsInShadow(int px, int py, int L, int W, int x, int y) {
-  return x <= px && px < x + W && y <= py && py < y + L;
+inline int Max(int a, int b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
 }
 
 int main() {
@@ -16,16 +17,20 @@ int main() {
     }
   }
 
-  for (int step = 0; step <= k; ++step, x -= s, y -= s) {
+  // Calculate the steps needed initially for the shadow to touch the sun.
+  // x - step * s < M => step > (x - M) / s
+  int x_step = (x - M) / s + 1;
+  int y_step = (y - N) / s + 1;
+  int step = Max(0, Max(x_step, y_step));
+  x -= step * s;
+  y -= step * s;
+
+  for (; step <= k; ++step, x -= s, y -= s) {
 #ifdef GP_DEBUG
     fprintf(stderr,
             "Bottom-left point of shadow after stepping %d steps: (%d, %d)\n",
             step, x, y);
 #endif
-    // The shadow still needs to move until it can reach the sun.
-    if (x >= M || y >= N) {
-      continue;
-    }
     // The shadow has already left the sun.
     if (x + W - 1 < 0 || y + L - 1 < 0) {
       break;
@@ -33,7 +38,8 @@ int main() {
 
     for (int j = N - 1; j >= 0; --j) {
       for (int i = 0; i < M; ++i) {
-        if (IsInShadow(i, j, L, W, x, y)) {
+        if (x <= i && i < x + W && y <= j && j < y + L) {
+          // Point (i, j) is in shadow.
           printf("0");
         } else {
           printf("%d", sun[i][j]);
