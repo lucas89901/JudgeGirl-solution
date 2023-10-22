20b1fc092ce7b1747d44879373f01e671ad147e2
Lucas Lu <lucas89901@gmail.com>
2023-10-22 22:12:20 +0800

    Update 10379. Permutation for new problem specification

--- C2023 For Strong/Quiz 3/10379. Permutation/permute.c
+++ C2023 For Strong/Quiz 3/10379. Permutation/permute.c
@@ -1,13 +1,15 @@
 #include "permute.h"
 
 #include <stddef.h>
-#include <stdio.h>
 
 void permute(int *ptrArray[MAX], int *ptrPerms[MAX]) {
-  int *array = ptrArray[0];
   for (int i = 0; ptrPerms[i] != NULL; ++i) {
+    int *old_ptr_array[MAX];
     for (int j = 0; ptrArray[j] != NULL; ++j) {
-      ptrArray[j] = array + ptrPerms[i][ptrArray[j] - array];
+      old_ptr_array[j] = ptrArray[j];
+    }
+    for (int j = 0; ptrArray[j] != NULL; ++j) {
+      ptrArray[j] = old_ptr_array[ptrPerms[i][j]];
     }
   }
 }
